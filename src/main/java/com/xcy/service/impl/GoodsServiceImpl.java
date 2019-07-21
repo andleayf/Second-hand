e665c74574b5d14f27ec11c34463362e1d743786
yemao <1270086576@qq.com>
2019-07-21 21:55:16 +0800

    修改了GoodsServiceImpl.java

--- src/main/java/com/xcy/service/impl/GoodsServiceImpl.java
+++ src/main/java/com/xcy/service/impl/GoodsServiceImpl.java
@@ -4,9 +4,11 @@ import com.xcy.mapper.GoodsMapper;
 import com.xcy.pojo.Goods;
 import com.xcy.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class GoodsServiceImpl implements GoodsService {
 
     @Autowired
